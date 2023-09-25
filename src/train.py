533c343d678edd6f8dae3c07b3ffadab1bd3c4f0
Edison <ejbejaranos@gmail.com>
2023-09-25 23:42:15 +0200

    Updating code

---
+++ src/train.py
@@ -0,0 +1,12 @@
+import os
+import re
+from typing import Optional
+from src.multimodal.architecture.late_fusion import LFLSTM
+
+
+import sys
+import datasets.CMU-MultimodalSDK.mmsdk
+from mmsdk import mmdatasdk as md
+from subprocess import check_call, CalledProcessError
+
+print("ya esta")

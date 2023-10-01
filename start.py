1bb0e543960aef07a8783fdf752f6948ca31d73b
Edison <ejbejaranos@gmail.com>
2023-10-02 01:32:20 +0200

    feat(start.py): add start.py script to automate installation of dependencies and execution of different options
    The start.py script is added to automate the installation of dependencies and provide a menu of options for executing different parts of the project. The script first installs the required dependencies by checking the operating system and using the appropriate package manager (brew, apt-get, or choco). Then, it presents a menu of options to the user to choose from. Each option corresponds to a different part of the project (01_Dataset_generation, 02_Model_Training, 03_Inference_Engine). The selected option is executed using the appropriate Python executable (python3 for Python 3.x and python for Python 2.x).

---
+++ start.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+def install_dependencies():
+    print("==== Instalando dependencias ====")
+    platform = sys.platform.lower()
+    if "darwin" in platform:  # macOS
+        os.system("brew install ffmpeg")
+    elif "linux" in platform:  # Linux
+        os.system("sudo apt-get install ffmpeg")
+    elif "win" in platform:  # Windows
+        os.system("choco install ffmpeg")
+    else:
+        print("Sistema operativo no compatible. Por favor, instale FFmpeg manualmente.")
+        return
+    
+    os.system("pip install -q -r ./01_Dataset_generation/requirements.txt")
+
+
+
+def execute_option(option):
+    python_executable = "python3" if sys.version_info >= (3, 0) else "python"
+    
+    if option == "1":
+        print(f"Ejecutando 01_Dataset_generation con {python_executable}...")
+        os.system(f"{python_executable} ./01_Dataset_generation/main.py")
+    elif option == "2":
+        print(f"Ejecutando 02_Model_Training con {python_executable}...")
+        os.system(f"{python_executable} ./02_Model_Training/main.py")
+    elif option == "3":
+        print(f"Ejecutando 03_Inference_Engine con {python_executable}...")
+        os.system(f"{python_executable} ./03_Inference_Engine/main.py")
+    else:
+        print("Opción no válida. Por favor, seleccione una opción válida.")
+
+def main():
+    install_dependencies()
+    print("==== Menú de Opciones ====")
+    print("1. Ejecutar 01_Dataset_generation")
+    print("2. Ejecutar 02_Model_Training")
+    print("3. Ejecutar 03_Inference_Engine")
+
+    option = input("Seleccione una opción (1/2/3): ")
+    execute_option(option)
+
+if __name__ == "__main__":
+    main()

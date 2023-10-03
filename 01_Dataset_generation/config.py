496606cb2291332f462f5e3c0fae4e446f9b7933
Edison <ejbejaranos@gmail.com>
2023-10-03 23:48:31 +0200

    chore(config.py): fix typo in dataset sources CSV paths and update comments for better readability

--- 01_Dataset_generation/config.py
+++ 01_Dataset_generation/config.py
@@ -4,22 +4,28 @@ SKIP_QUESTIONS = False  # SKIP AT YOUR OWN RISK
 DATASET_FOLDER_PATH = "dataset"
 DESTINATION_FOLDER_PATH = "preprocessed_dataset"
 
-#VIDEO
+# VIDEO
 TARGET_VIDEO_FPS = 3
 TARGET_VIDEO_RESOLUTION_SCALE = 0.7
 SPLIT_VIDEO_WHEN_THERES_SILENCE = True
 MIN_VIDEO_CHUNK_TIME = 0.5
-#AUDIO
+# AUDIO
 REMOVE_AUDIO_FROM_PREPROCESSED_VIDEOS = False
 TARGET_AUDIO_SAMPLERATE = 8000
 TARGET_AUDIO_BITRATE = "128k"
 
-
-
-#good :
+# DATASETS
+SOURCES_CSV_PATHS = {
+    "meld": [
+        "./datset_adapters/MELD/dev_sent_emo.csv",
+        "./datset_adapters/MELD/test_sent_emo.csv",
+        "./datset_adapters/MELD/train_sent_emo.csv"
+    ]
+}
+# good :
 # -trans: me fue muy bien (positive)
 # -img: :) (positive)
 
-#video_splits :
+# video_splits :
 # -trans: me fue muy bien (negative)
 # -img: :( (negative)

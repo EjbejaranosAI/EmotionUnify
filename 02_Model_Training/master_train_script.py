90e381d26c15377ae2e9e054d6b120983a132a0d
Edison <ejbejaranos@gmail.com>
2023-10-03 13:11:47 +0200

    refactor(master_train_script.py): reorganize the code into a class-based pipeline for better modularity and readability feat(master_train_script.py): add methods to read metadata and videos, extract text, audio, and video features, concatenate features, make predictions using classifier or BLSTM, and store weights and models

--- 02_Model_Training/master_train_script.py
+++ 02_Model_Training/master_train_script.py
@@ -14,3 +14,76 @@ IS GOING TO BE ORGANIZZED IN THE NEXT WAY THE PIPELINE:
 5. Made prediction by two options (Classifier or BLSTM)
 6. Store weigths and models
 """
+
+import configparser
+from feature_extraction import text_features, audio_features, video_features
+from utilities import data_loader, read_metadata, store_models
+from prediction import blstm_classifier, classifier
+
+class MultimodalTrainPipeline:
+    def __init__(self, config_file):
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file)
+
+    def read_metadata_and_videos(self):
+        """Read metadata and videos."""
+        try:
+            data_loader.load()
+        except Exception as e:
+            print(f"Error in read_metadata_and_videos: {e}")
+
+    def extract_text_features(self):
+        """Extract features from text modality."""
+        try:
+            text_features.extract()
+        except Exception as e:
+            print(f"Error in extract_text_features: {e}")
+
+    def extract_audio_features(self):
+        """Extract features from audio modality."""
+        try:
+            audio_features.extract()
+        except Exception as e:
+            print(f"Error in extract_audio_features: {e}")
+
+    def extract_video_features(self):
+        """Extract features from video modality."""
+        try:
+            video_features.extract()
+        except Exception as e:
+            print(f"Error in extract_video_features: {e}")
+
+    def concatenate_features(self, mode='concatenate'):
+        """Concatenate extracted features."""
+        try:
+            # Implement feature concatenation here
+            pass
+        except Exception as e:
+            print(f"Error in concatenate_features: {e}")
+
+    def make_prediction(self, mode='Classifier'):
+        """Make predictions using Classifier or BLSTM."""
+        try:
+            if mode == 'Classifier':
+                classifier.predict()
+            else:
+                blstm_classifier.predict()
+        except Exception as e:
+            print(f"Error in make_prediction: {e}")
+
+    def store_weights_and_models(self):
+        """Store model weights and configurations."""
+        try:
+            store_models.store()
+        except Exception as e:
+            print(f"Error in store_weights_and_models: {e}")
+
+if __name__ == "__main__":
+    pipeline = MultimodalTrainPipeline('config.ini')
+    pipeline.read_metadata_and_videos()
+    pipeline.extract_text_features()
+    pipeline.extract_audio_features()
+    pipeline.extract_video_features()
+    pipeline.concatenate_features()
+    pipeline.make_prediction()
+    pipeline.store_weights_and_models()

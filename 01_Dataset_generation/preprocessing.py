676b9e499e1926a17c116bb1981cb86b670f82d8
Edison <ejbejaranos@gmail.com>
2023-10-03 10:59:18 +0200

    refactor(preprocessing.py): rename destination folder from "preprocessing_files" to "preprocessed_dataset" for better clarity and semantics refactor(preprocessing.py): update console log messages to reflect the new destination folder name "preprocessed_dataset"

--- 01_Dataset_generation/preprocessing.py
+++ 01_Dataset_generation/preprocessing.py
@@ -10,12 +10,12 @@ class Preprocess:
 
     def create_folder_environment(self):
         source_path = self.path
-        destination_path = os.path.join(os.getcwd(), "preprocessing_files")
+        destination_path = os.path.join(os.getcwd(), "preprocessed_dataset")
 
         if os.path.exists(destination_path):
             files_in_destination = os.listdir(destination_path)
             if not config.SKIP_QUESTIONS and files_in_destination:
-                response = input("║  The 'preprocessing_files' folder already exists and its NOT empty.\n"
+                response = input("║  The 'preprocessed_dataset' folder already exists and its NOT empty.\n"
                                  "║  ⚠️Do you want to remove the existing folder and regenerate it? (y/n): ")
                 if "y" in response.lower():
                     shutil.rmtree(destination_path)
@@ -32,9 +32,9 @@ class Preprocess:
             os.makedirs(destination_folder_path, exist_ok=True)
 
     def preprocess_dataset(self):
-        print("====> Down sampling data and moving it to the '/preprocessing_files' folder")
+        print("====> Down sampling data and moving it to the '/preprocessed_dataset' folder")
         source_path = self.path
-        destination_path = os.path.join(os.getcwd(), "preprocessing_files")
+        destination_path = os.path.join(os.getcwd(), "preprocessed_dataset")
         folders = [f for f in os.listdir(source_path) if os.path.isdir(os.path.join(source_path, f))]
         with tqdm(total=len(folders), desc="Down sampling files", unit="folder") as pbar:
             for folder in folders:

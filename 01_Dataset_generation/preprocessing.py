fff7a6db917cb27f41161272c501896be318d503
Edison <ejbejaranos@gmail.com>
2023-10-05 07:21:29 +0200

    fix(preprocessing.py): remove unnecessary break statement in Preprocess class
    The break statement was causing the loop to exit prematurely, preventing the execution of the VideoPreprocessing class for all files in the folder. Removing the break statement ensures that the VideoPreprocessing class is executed for each file in the folder.

--- 01_Dataset_generation/preprocessing.py
+++ 01_Dataset_generation/preprocessing.py
@@ -45,7 +45,7 @@ class Preprocess:
                     file_path = os.path.join(folder_path, file)
                     destination_file_path = os.path.join(destination_folder_path, file)
                     VideoPreprocessing(file_path).execute(destination_file_path)
-                    break
+
                 pbar.update(1)
 
     def execute(self):

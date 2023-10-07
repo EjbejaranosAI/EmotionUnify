aa21577ac5a95932a476be89420364268c0423f0
Edison <ejbejaranos@gmail.com>
2023-10-07 02:18:58 +0200

    🔧 chore(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics 🔧 chore(server.ts): add support for process.env.PORT environment variable to be able to run app on a configurable port 🔧 chore(MELD_dataset_adapter.py): add MELD dataset adapter script 🔧 chore(MELD_dataset_adapter.py): add function for printing a formatted title 🔧 chore(MELD_dataset_adapter.py): add function for printing global summary 🔧 chore(MELD_dataset_adapter.py): add function for downloading MELD dataset 🔧 chore(MELD_dataset_adapter.py): add VideoProcessor class 🔧 chore(MELD_dataset_adapter.py): add generate_new_filename method to VideoProcessor class 🔧 chore(MELD_dataset_adapter.py): add print_summary method to VideoProcessor class 🔧 chore(MELD_dataset_adapter.py): add process_videos method to VideoProcessor class 🔧 chore(MELD_dataset_adapter.py): add adapt_meld_dataset function 🔧 chore(MELD_dataset_adapter.py): add execute_option function 🔧 chore(MELD_dataset_adapter.py): add main function 🔧 chore(audio_features.py): add Resample import 🔧 chore(audio_features.py): add soundfile import 🔧 chore(audio_features.py): add torch.nn import 🔧 chore(audio_features.py): add torch.nn.functional import 🔧 chore(audio_features.py): add Wav2Vec2Model import 🔧 chore(audio_features.py): add Wav2Vec2PreTrainedModel import 🔧 chore(audio_features.py): add os import 🔧 chore(audio_features.py): add pandas import 🔧 chore(audio_features.py): add numpy import 🔧 chore(audio_features.py): add RegressionHead class 🔧 chore(audio_features.py): add CustomEmotionModel class 🔧 chore(audio_features.py): add settings dictionary 🔧 chore(audio_features.py): add processor initialization 🔧 chore(audio_features.py): add model initialization 🔧 chore(audio_features.py): add resampler initialization 🔧 chore(audio_features.py): add read_and_preprocess_audio function 🔧 chore(audio_features.py): add process_audio function 🔧 chore(audio_features.py): add extract_features_and_labels function 🔧 chore

---
+++ 01_Dataset_generation/dataset_adapters/MELD_dataset_adapter.py
@@ -0,0 +1,254 @@
+import os
+import pandas as pd
+import os.path
+from os import path
+import logging
+from tqdm import tqdm
+import shutil
+import subprocess
+import tarfile
+import shutil
+
+OUTPUT_DIRECTORY = "../preprocessed_dataset"
+OUTPUT_TRAIN_DIR = "../dataset"
+
+# Function for printing a formatted title
+def print_title(title):
+    border = '═' * (len(title) + 2)
+    print(f"\n    ╔{border}╗")
+    print(f"    ║ {title} ║")
+    print(f"    ╚{border}╝\n")
+
+
+def print_global_summary(global_summary):
+    print("\nGlobal Summary:")
+    header = "║ Emotion/Sentiment  Train  Dev  Test  Total ║"
+    header_len = len(header)
+
+    # Calculate the width of the table dynamically
+    table_width = header_len - 2  # Exclude the side borders
+
+    print(f"╔{'═' * table_width}╗")
+    print(f"║{'Emotion/Sentiment Summary'.center(table_width)}║")
+    print(f"╠{'═' * table_width}╣")
+    print(header)
+    print(f"╠{'═' * table_width}╣")
+
+    for emotion, counts in global_summary.items():
+        row_str = f"║ {emotion: <16} {counts['train']: >5} {counts['dev']: >5} {counts['test']: >5} {counts['total']: >6} ║"
+        print(row_str)
+
+    print(f"╚{'═' * table_width}╝")
+
+
+def download_meld_dataset():
+    if not path.exists("./MELD"):
+        os.mkdir("./MELD")
+
+    try:
+        logging.basicConfig(filename='download.log', level=logging.INFO)
+        logging.info('Downloading MELD dataset')
+
+        try:
+            subprocess.run(['wget', 'https://huggingface.co/datasets/declare-lab/MELD/resolve/main/MELD.Raw.tar.gz'])
+        except:
+            subprocess.run(['wget', 'http://web.eecs.umich.edu/~mihalcea/downloads/MELD.Raw.tar.gz'])
+
+        # Use tqdm to track the progress of extracting files
+        with tqdm(total=100, unit="%", desc="Extracting files", ascii=True) as progress_bar:
+            subprocess.run(['tar', '-xvf', 'MELD.Raw.tar.gz'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            progress_bar.update(100)  # Mark as complete
+            print("✅ Extraction complete.")
+
+        os.remove('MELD.Raw.tar.gz')
+        os.rename('MELD.Raw', 'MELD')
+
+        files = os.listdir("./MELD")
+        files_to_uncompress = []
+        for file in files:
+            if file.endswith('.tar.gz'):
+                print("📦 tar.gz file found")
+                files_to_uncompress.append(file)
+        if len(files_to_uncompress) > 0:
+            for file in files_to_uncompress:
+                # Extract files and delete tar.gz files
+                tar = tarfile.open("./MELD/"+file)
+                with tqdm(total=100, unit="%", desc=f"Extracting {file}", ascii=True) as inner_progress_bar:
+                    tar.extractall("./MELD/")
+                    inner_progress_bar.update(100)  # Mark as complete
+                tar.close()
+                os.remove("./MELD/"+file)
+                print(f"📦 {file} extraction complete.")
+        else:
+            print("No tar.gz files found")
+    except Exception as e:
+        logging.error('Error in downloading MELD dataset')
+        print('❌ Error in downloading MELD dataset:', str(e))
+
+
+
+class VideoProcessor:
+    def __init__(self, csv_path, video_directory, output_directory, dataset_type, join_sets):
+        self.csv_path = csv_path
+        self.video_directory = video_directory
+        self.join_sets = join_sets
+        self.dataset_type = dataset_type if not join_sets else 'joined'
+        self.output_directory = output_directory
+
+    def generate_new_filename(self, video_id, dialogue_id, utterance_id):
+        return f"{video_id}_{dialogue_id}_{utterance_id}.mp4"
+
+    def print_summary(self, classification_type):
+        print(f"\n{classification_type} Summary:")
+        summary_dict = {}
+        for folder in os.listdir(self.output_directory):
+            folder_path = os.path.join(self.output_directory, folder)
+            if os.path.isdir(folder_path):
+                num_files = len(os.listdir(folder_path))
+                summary_dict[folder] = num_files
+
+        max_key_length = max([len(key) for key in summary_dict.keys()])
+        max_value_length = max([len(str(value)) for value in summary_dict.values()])
+        total_length = max_key_length + max_value_length + 6  # 6 for extra spaces and special characters
+
+        print(f"\n{'═' * (total_length + 2)}")
+        print("║         Distribution           ║")
+        print(f"{'═' * (total_length + 2)}")
+        for key, value in summary_dict.items():
+            print(f"║ {key: <{max_key_length}} {value: >{max_value_length}} ║")
+        print(f"{'═' * (total_length + 2)}")
+
+    def process_videos(self, classification_type, global_summary, join_datasets=False):
+        df = pd.read_csv(self.csv_path)
+        local_summary = {}
+
+        for row in df.itertuples():
+            dialogue_id = row.Dialogue_ID
+            utterance_id = row.Utterance_ID
+            video_id = f"dia{dialogue_id}_utt{utterance_id}.mp4"
+
+            if classification_type == "Emotion":
+                category = row.Emotion
+            elif classification_type == "Sentiment":
+                category = row.Sentiment
+            else:
+                print("❌ Invalid classification type.")
+                return
+
+            if join_datasets:
+                destination_folder = os.path.join(self.output_directory, "joined", category)
+
+            else:
+                destination_folder = os.path.join(self.output_directory, self.dataset_type, category)
+
+            os.makedirs(destination_folder, exist_ok=True)
+
+            source_video_path = os.path.join(self.video_directory, video_id)
+            destination_video_path = os.path.join(destination_folder, video_id)
+
+            try:
+                shutil.move(source_video_path, destination_video_path)
+                local_summary[category] = local_summary.get(category, 0) + 1
+                global_summary[category][self.dataset_type if not join_datasets else 'joined'] += 1
+                global_summary[category]['total'] += 1
+            except FileNotFoundError:
+                print(f"❌ Video {video_id} not found.")
+
+        print("Process completed.")
+
+
+def adapt_meld_dataset():
+    global_summary = {}
+
+    csv_paths = ['./MELD/dev_sent_emo.csv', './MELD/train_sent_emo.csv', './MELD/test_sent_emo.csv']
+    video_directories = ['./MELD/dev_splits_complete', './MELD/train_splits', './MELD/output_repeated_splits_test']
+    dataset_types = ['dev', 'train', 'test']
+    output_directory = OUTPUT_DIRECTORY
+
+    print("Classification Types:")
+    print("1. Emotion")
+    print("2. Sentiment")
+    choice = input("Choose classification type (1/2): ")
+    classification_type = "Emotion" if choice == "1" else "Sentiment"
+
+    for category in ["neutral", "joy", "sadness", "anger", "surprise", "disgust", "fear", "positive", "negative"]:
+        global_summary[category] = {'train': 0, 'dev': 0, 'test': 0, 'total': 0}
+
+    join_choice = input("Do you want to join train, dev, and test sets? (y/n): ")
+    join_sets = True if join_choice.lower() == 'y' else False
+
+    # Moved the directory deletion logic here
+    if os.path.exists(output_directory):
+        if join_sets:  # Only delete if join_sets is True
+            shutil.rmtree(output_directory)
+    os.makedirs(output_directory, exist_ok=True)
+
+    # Add 'joined' key to global_summary if datasets are to be joined
+    if join_sets:
+        for category in global_summary.keys():
+            global_summary[category]['joined'] = 0
+
+    for csv_path, video_directory, dataset_type in zip(csv_paths, video_directories, dataset_types):
+        print(f"\nProcessing {csv_path} and videos from {video_directory}")
+        processor = VideoProcessor(csv_path, video_directory, output_directory, dataset_type, join_sets)
+        processor.process_videos(classification_type, global_summary)
+
+    print_global_summary(global_summary)
+
+    if not join_sets:
+        train_dir_source = os.path.join(OUTPUT_DIRECTORY, 'train')
+
+        # Check if the source directory exists
+        if os.path.exists(train_dir_source):
+            # Create destination directory if it doesn't exist
+            if not os.path.exists(OUTPUT_TRAIN_DIR):
+                os.makedirs(OUTPUT_TRAIN_DIR)
+            for subfolder in os.listdir(train_dir_source):
+                subfolder_source = os.path.join(train_dir_source, subfolder)
+                subfolder_destination = os.path.join(OUTPUT_TRAIN_DIR, subfolder)
+                if os.path.exists(subfolder_destination):
+                    shutil.rmtree(subfolder_destination)
+                # Move the subfolder
+                shutil.move(subfolder_source, subfolder_destination)
+                print(f"✅ Moved {subfolder} to {OUTPUT_TRAIN_DIR}")
+
+
+def execute_option(option):
+    if option == "1":
+        print_title("Downloading MELD Dataset")
+        download_meld_dataset()  # Uncomment this if you want to download the dataset
+        #print_title("Adapting MELD Dataset")
+        #adapt_meld_dataset()
+
+    elif option == "2":
+        print_title("Adapting MELD Dataset")
+        adapt_meld_dataset()  # Call the function to adapt the dataset
+
+    elif option == "3":
+        print_title("Clean directories")
+
+        # Delete the videos_procesados directory
+        output_directory = OUTPUT_DIRECTORY
+        if os.path.exists(output_directory):
+            shutil.rmtree(output_directory)
+        print(f"✅ Deleted directory: {output_directory}")
+
+        # Delete the MELD directory
+        meld_directory = './MELD'
+        if os.path.exists(meld_directory):
+            shutil.rmtree(meld_directory)
+        print(f"✅ Deleted directory: {meld_directory}")
+
+    else:
+        print("❌ Invalid option.")
+
+
+
+if __name__ == "__main__":
+    print_title("🚀 Welcome to the PREPROCESSING PIPELINE! 🚀")
+    print("📦 1️⃣ Download MELD dataset and adapt MELD 📥")
+    print("🔄 2️⃣ Only Adapt MELD dataset to the framework 🛠️")
+    print("🔙 3️⃣ Undo & Clean Up (Deletes processed directories)")
+
+    option = input("👉 Select an option (1/2/3): 👈")
+    execute_option(option)
--- 02_Model_Training/feature_extraction/audio_features.py
+++ 02_Model_Training/feature_extraction/audio_features.py
@@ -1,65 +1,126 @@
+from torchaudio.transforms import Resample
+import soundfile as sf
 import torch
-import configparser
-import torchaudio
-from transformers import Wav2Vec2Processor
+import torch.nn as nn
+import torch.nn.functional as nnf
 from tqdm import tqdm
+from transformers import Wav2Vec2Processor, Wav2Vec2Model, Wav2Vec2PreTrainedModel
+import os
+import pandas as pd
+import numpy as np
 
 
-class AudioFeatureExtractor:
-    def __init__(self, classification_type="Emotion"):
-        config = configparser.ConfigParser()
-        config.read('../config.ini')
-        self.model_name = config.get('AudioFeatureExtraction', 'model_name')
+# Custom Regression Head
+class RegressionHead(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.dropout = nn.Dropout(config.final_dropout)
+        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
 
-        self.processor = Wav2Vec2Processor.from_pretrained(self.model_name)
-        self.model = EmotionModel.from_pretrained(self.model_name)
-        self.classification_type = classification_type
+    def forward(self, features, **kwargs):
+        x = features
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = self.out_proj(x)
+        return x
 
-    def mapping_emotion(self, df):
-        # Your code for mapping emotions or other labels
-        pass
 
-    def extract_features_from_audio(self, audio_path, sampling_rate):
-        waveform, _ = torchaudio.load(audio_path)
-        return self.process_audio(waveform, sampling_rate)
+# Custom Model that includes Wav2Vec2 and Regression Head
+class CustomEmotionModel(Wav2Vec2PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.wav2vec2 = Wav2Vec2Model(config)
+        self.classifier = RegressionHead(config)
+        self.init_weights()
 
-    def process_audio(self, wavs, sampling_rate):
-        y = self.processor([wav.cpu().numpy() for wav in wavs],
-                           sampling_rate=sampling_rate,
-                           return_tensors="pt",
-                           padding="longest")
+    def forward(self, input_values):
+        outputs = self.wav2vec2(input_values)
+        hidden_states = outputs[0]
+        hidden_states = torch.mean(hidden_states, dim=1)
+        logits = self.classifier(hidden_states)
+        return hidden_states, logits
 
-        y = y['input_values']
 
-        with torch.no_grad():
-            output = self.model(y)
+# Configuration
+settings = {
+    'model_name': 'audeering/wav2vec2-large-robust-12-ft-emotion-msp-dim',
+    'device': 'cpu',
+    'resample_orig_freq': 48000,
+    'resample_new_freq': 16000,
+    'min_length': 16000
+}
 
-        return {
-            'hidden_states': output[0],
-            'logits': output[1],
-        }
+# Initialize model and processor
+processor = Wav2Vec2Processor.from_pretrained(settings['model_name'])
+model = CustomEmotionModel.from_pretrained(settings['model_name']).to(settings['device'])
 
-    def extract_features_from_folder(self, audio_folder_path, df):
-        audio_feature_dict = {}
-        audio_files_to_process = [file for file in os.listdir(audio_folder_path) if file in set(df['audio_id'])]
+# Initialize resampler
+resampler = Resample(settings['resample_orig_freq'], settings['resample_new_freq'])
 
-        for audio_file in tqdm(audio_files_to_process):
-            audio_path = os.path.join(audio_folder_path, audio_file)
-            features = self.extract_features_from_audio(audio_path, 16000)  # Replace 16000 with actual sampling rate
-            audio_feature_dict[audio_file] = features
 
-        return audio_feature_dict
+# Helper functions
+def read_and_preprocess_audio(file_path):
+    audio_data, _ = sf.read(file_path)
+    if audio_data.ndim > 1:
+        audio_data = audio_data.mean(axis=1)
+    audio_data_resampled = resampler(torch.tensor(audio_data).float())
+    if audio_data_resampled.shape[0] < settings['min_length']:
+        audio_data_resampled = nnf.pad(audio_data_resampled,
+                                       (0, settings['min_length'] - audio_data_resampled.shape[0]))
+    return audio_data_resampled
 
-    def save_features(self, features, filename):
-        torch.save(features, f'{filename}_audio_features.pth')
 
-# Keep your existing EmotionModel and RegressionHead classes here
+def process_audio(audio_data):
+    audio_input = processor(audio_data.cpu().numpy(), sampling_rate=settings['resample_new_freq'], return_tensors="pt",
+                            padding="longest")['input_values']
+    audio_input = audio_input.to(settings['device'])
+    with torch.no_grad():
+        outputs = model(audio_input)
+    return {'hidden_states': outputs[0], 'logits': outputs[1]}
+
+
+def extract_features_and_labels(audio_dir, csv_path, classification_type):
+    feature_dict = {}
+    df = pd.read_csv(csv_path)
+    df['video_id'] = "dia" + df['Dialogue_ID'].astype(str) + '_utt' + df['Utterance_ID'].astype(str) + '.wav'
+
+    if classification_type == "Emotion":
+        df = mapping_emotion(df)
+    elif classification_type == "Sentiment":
+        df = mapping_sentiment(df)
+
+    label_column = f"{classification_type}_encoded"
+
+    for audio_file in tqdm(os.listdir(audio_dir)):
+        if audio_file.endswith('.wav'):
+            audio_path = os.path.join(audio_dir, audio_file)
+            audio_data = read_and_preprocess_audio(audio_path)
+            features = process_audio(audio_data)
+            label = df[df['video_id'] == audio_file][label_column].values[0]
+            feature_dict[audio_file] = {'features': features['hidden_states'].detach().cpu().numpy(), 'label': label}
+    return feature_dict
 
 
 if __name__ == "__main__":
-    feature_extractor = AudioFeatureExtractor("Emotion")
-    audio_folder_path = "/path/to/audio/files"
-    df = ...  # Your DataFrame
+    csv_train = "/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/train_sent_emo.csv"
+    csv_dev = "/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/dev_sent_emo.csv"
+    csv_test = "/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/test_sent_emo.csv"
 
-    extracted_features = feature_extractor.extract_features_from_folder(audio_folder_path, df)
-    feature_extractor.save_features(extracted_features, "train")
+    feature_dict_train = extract_features_and_labels(
+        "/01_Dataset_generation/dataset_adapters/MELD/train_splits_complete_wav", csv_train,
+                                                     "Emotion")
+    np.save('./audio_features/audio__train_features.npy', feature_dict_train)
+
+    feature_dict_dev = extract_features_and_labels(
+        "/01_Dataset_generation/dataset_adapters/MELD/dev_splits_complete_wav", csv_dev,
+                                                   "Emotion")
+    np.save('./audio_features/audio__dev_features.npy', feature_dict_dev)
+
+    feature_dict_test = extract_features_and_labels(
+        "/01_Dataset_generation/dataset_adapters/MELD/output_repeated_splits_test_wav", csv_test,
+                                                    "Emotion")
+    np.save('./audio_features/audio__test_features.npy', feature_dict_test)
+

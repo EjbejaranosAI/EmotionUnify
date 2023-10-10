fe0ad2a2a151ecc588a3e8d86b83b1533ac0c338
Edison <ejbejaranos@gmail.com>
2023-10-10 22:36:19 +0200

    🔧 refactor(audio_features.py): reorganize imports and remove unused code to improve code readability and maintainability ✨ feat(audio_features.py): add support for extracting audio features from video files and saving them as pickle files for further processing

--- 02_Model_Training/feature_extraction/audio_features.py
+++ 02_Model_Training/feature_extraction/audio_features.py
@@ -1,126 +1,125 @@
-from torchaudio.transforms import Resample
-import soundfile as sf
-import torch
-import torch.nn as nn
-import torch.nn.functional as nnf
-from tqdm import tqdm
-from transformers import Wav2Vec2Processor, Wav2Vec2Model, Wav2Vec2PreTrainedModel
-import os
 import pandas as pd
 import numpy as np
-
-
-# Custom Regression Head
-class RegressionHead(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        self.dropout = nn.Dropout(config.final_dropout)
-        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
-
-    def forward(self, features, **kwargs):
-        x = features
-        x = self.dropout(x)
-        x = self.dense(x)
-        x = torch.tanh(x)
-        x = self.dropout(x)
-        x = self.out_proj(x)
-        return x
-
-
-# Custom Model that includes Wav2Vec2 and Regression Head
-class CustomEmotionModel(Wav2Vec2PreTrainedModel):
-    def __init__(self, config):
-        super().__init__(config)
-        self.wav2vec2 = Wav2Vec2Model(config)
-        self.classifier = RegressionHead(config)
-        self.init_weights()
-
-    def forward(self, input_values):
-        outputs = self.wav2vec2(input_values)
-        hidden_states = outputs[0]
-        hidden_states = torch.mean(hidden_states, dim=1)
-        logits = self.classifier(hidden_states)
-        return hidden_states, logits
-
+import torch
+from moviepy.editor import VideoFileClip
+from transformers import Wav2Vec2Processor, Wav2Vec2Model
+import os
+import pickle
+from tqdm import tqdm
+from torchaudio.transforms import Resample
 
 # Configuration
 settings = {
     'model_name': 'audeering/wav2vec2-large-robust-12-ft-emotion-msp-dim',
-    'device': 'cpu',
+    'device': 'cuda' if torch.cuda.is_available() else 'cpu',
     'resample_orig_freq': 48000,
     'resample_new_freq': 16000,
     'min_length': 16000
 }
 
-# Initialize model and processor
-processor = Wav2Vec2Processor.from_pretrained(settings['model_name'])
-model = CustomEmotionModel.from_pretrained(settings['model_name']).to(settings['device'])
+class EmotionModel(Wav2Vec2PreTrainedModel):
+    r"""Speech emotion classifier."""
 
-# Initialize resampler
-resampler = Resample(settings['resample_orig_freq'], settings['resample_new_freq'])
+    def __init__(self, config):
+
+        super().__init__(config)
+
+        self.config = config
+        self.wav2vec2 = Wav2Vec2Model(config)
+        self.classifier = RegressionHead(config)
+        self.init_weights()
+
+    def forward(
+            self,
+            input_values,
+    ):
+
+        outputs = self.wav2vec2(input_values)
+        hidden_states = outputs[0]
+        hidden_states = torch.mean(hidden_states, dim=1)
+        logits = self.classifier(hidden_states)
+
+        return hidden_states, logits
+
+class AudioFeatureExtractor:
+    def __init__(self, settings, classification_type="Sentiment"):
+        self.processor = Wav2Vec2Processor.from_pretrained(settings['model_name'])
+        self.model = EmotionModel.from_pretrained(settings['model_name']).to(settings['device'])
+        self.model.eval()
+        self.resampler = Resample(settings['resample_orig_freq'], settings['resample_new_freq'])
+        self.classification_type = classification_type
 
 
-# Helper functions
-def read_and_preprocess_audio(file_path):
-    audio_data, _ = sf.read(file_path)
-    if audio_data.ndim > 1:
-        audio_data = audio_data.mean(axis=1)
-    audio_data_resampled = resampler(torch.tensor(audio_data).float())
-    if audio_data_resampled.shape[0] < settings['min_length']:
-        audio_data_resampled = nnf.pad(audio_data_resampled,
-                                       (0, settings['min_length'] - audio_data_resampled.shape[0]))
-    return audio_data_resampled
+    def mapping_labels(self, df):
+        mapping = {
+            'Emotion': {
+                'neutral': [1, 0, 0, 0, 0, 0, 0],
+                'surprise': [0, 1, 0, 0, 0, 0, 0],
+                'fear': [0, 0, 1, 0, 0, 0, 0],
+                'sadness': [0, 0, 0, 1, 0, 0, 0],
+                'joy': [0, 0, 0, 0, 1, 0, 0],
+                'disgust': [0, 0, 0, 0, 0, 1, 0],
+                'anger': [0, 0, 0, 0, 0, 0, 1],
+            },
+            'Sentiment': {
+                'neutral': [1, 0, 0],
+                'positive': [0, 1, 0],
+                'negative': [0, 0, 1]
+            }
+        }
+        label_column = f"{self.classification_type}_encoded"
+        df[label_column] = df[self.classification_type].map(mapping[self.classification_type])
+        return df
 
+    def extract_and_save_features(self, video_folder_path, video_path_csv, set_name):
+        df = pd.read_csv(video_path_csv)
+        df['video_id'] = "dia" + df['Dialogue_ID'].astype(str) + '_utt' + df['Utterance_ID'].astype(str) + '.mp4'
+        df = self.mapping_labels(df)
 
-def process_audio(audio_data):
-    audio_input = processor(audio_data.cpu().numpy(), sampling_rate=settings['resample_new_freq'], return_tensors="pt",
-                            padding="longest")['input_values']
-    audio_input = audio_input.to(settings['device'])
-    with torch.no_grad():
-        outputs = model(audio_input)
-    return {'hidden_states': outputs[0], 'logits': outputs[1]}
+        audio_feature_dict = {}
+        video_files_to_process = [file for file in os.listdir(video_folder_path) if file in set(df['video_id'])]
 
+        for video_file in tqdm(video_files_to_process):
+            try:  # Handling corrupted or problematic video/audio files
+                video_path = os.path.join(video_folder_path, video_file)
+                clip = VideoFileClip(video_path)
+                audio = clip.audio.to_soundarray(fps=settings['resample_new_freq'])
+                audio = audio.mean(axis=1)
+                audio = self.resampler(torch.tensor(audio).float())
 
-def extract_features_and_labels(audio_dir, csv_path, classification_type):
-    feature_dict = {}
-    df = pd.read_csv(csv_path)
-    df['video_id'] = "dia" + df['Dialogue_ID'].astype(str) + '_utt' + df['Utterance_ID'].astype(str) + '.wav'
+                input_values = self.processor(audio.cpu().numpy(), sampling_rate=settings['resample_new_freq'], return_tensors="pt").input_values
+                input_values = input_values.to(settings['device'])
 
-    if classification_type == "Emotion":
-        df = mapping_emotion(df)
-    elif classification_type == "Sentiment":
-        df = mapping_sentiment(df)
+                with torch.no_grad():
+                    hidden_states, logits = self.model(input_values)
 
-    label_column = f"{classification_type}_encoded"
+                # Storing features as an array instead of a single value
+                aggregated_features = hidden_states.squeeze().cpu().numpy()
 
-    for audio_file in tqdm(os.listdir(audio_dir)):
-        if audio_file.endswith('.wav'):
-            audio_path = os.path.join(audio_dir, audio_file)
-            audio_data = read_and_preprocess_audio(audio_path)
-            features = process_audio(audio_data)
-            label = df[df['video_id'] == audio_file][label_column].values[0]
-            feature_dict[audio_file] = {'features': features['hidden_states'].detach().cpu().numpy(), 'label': label}
-    return feature_dict
+                # Explicitly setting dtype for label as np.int
+                label = np.array(df[df['video_id'] == video_file][f"{self.classification_type}_encoded"].values[0], dtype=np.int)
+
+                audio_feature_dict[video_file.split('.')[0]] = {'features': aggregated_features, 'label': label}
+
+            except Exception as e:
+                print(f"Skipping {video_file} due to error: {e}")
+
+        os.makedirs('./audio_features', exist_ok=True)
+        with open(f'./audio_features/{set_name}_audio_features.pkl', 'wb') as f:
+            pickle.dump(audio_feature_dict, f)
+
 
 
 if __name__ == "__main__":
-    csv_train = "/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/train_sent_emo.csv"
-    csv_dev = "/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/dev_sent_emo.csv"
-    csv_test = "/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/test_sent_emo.csv"
+    feature_extractor = AudioFeatureExtractor(settings, classification_type="Sentiment")
 
-    feature_dict_train = extract_features_and_labels(
-        "/01_Dataset_generation/dataset_adapters/MELD/train_splits_complete_wav", csv_train,
-                                                     "Emotion")
-    np.save('./audio_features/audio__train_features.npy', feature_dict_train)
 
-    feature_dict_dev = extract_features_and_labels(
-        "/01_Dataset_generation/dataset_adapters/MELD/dev_splits_complete_wav", csv_dev,
-                                                   "Emotion")
-    np.save('./audio_features/audio__dev_features.npy', feature_dict_dev)
+    dataset_paths = {
+        'Train': ('/01_Dataset_generation/dataset_adapters/MELD/train_splits', '/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/train_sent_emo.csv'),
+        'Test': ('/01_Dataset_generation/dataset_adapters/MELD/output_repeated_splits_test', '/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/test_sent_emo.csv'),
+        'Dev': ('/01_Dataset_generation/dataset_adapters/MELD/dev_splits_complete', '/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/dev_sent_emo.csv')
+    }
 
-    feature_dict_test = extract_features_and_labels(
-        "/01_Dataset_generation/dataset_adapters/MELD/output_repeated_splits_test_wav", csv_test,
-                                                    "Emotion")
-    np.save('./audio_features/audio__test_features.npy', feature_dict_test)
+    for set_name, (video_folder, csv_path) in dataset_paths.items():
+        feature_extractor.extract_and_save_features(video_folder, csv_path, set_name)
 

1cbbeee399c98777c0c280a6e3d60d326a02f8bf
Edison <ejbejaranos@gmail.com>
2023-10-17 12:45:51 +0200

    🐛 fix(master_train_script.py): fix load_config method not being called in the constructor ✨ feat(master_train_script.py): add tracking of emissions and time for feature extraction methods to provide insights on resource usage and environmental impact 🚀 chore(master_train_script.py): add initialization message to indicate start of script execution 🎉 feat(master_train_script.py): print completion message after feature extraction is complete

--- 02_Model_Training/master_train_script.py
+++ 02_Model_Training/master_train_script.py
@@ -6,9 +6,11 @@ from feature_extraction.video_features import VisionFeatureExtractor
 from feature_extraction.audio_features import AudioFeatureExtractor
 
 
-
 class MultimodalTrainPipeline:
     def __init__(self, config_file=None):
+        self.load_config(config_file)
+
+    def load_config(self, config_file):
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
         self.output_path = self.config['General'].get('output_path')
@@ -16,58 +18,35 @@ class MultimodalTrainPipeline:
         config_audio = self.config['AudioFeatureExtraction']
         self.text_feature_extractor = TextFeatureExtractor(classification_type)
         self.vision_feature_extractor = VisionFeatureExtractor(classification_type)
-        self.audio_feature_extractor = AudioFeatureExtractor(config_audio,classification_type)
+        self.audio_feature_extractor = AudioFeatureExtractor(config_audio, classification_type)
 
+    def track_emissions_and_time(self, func, *args, **kwargs):
+        tracker = EmissionsTracker(project_name=f"{func.__name__}")
+        start_time = time.time()
+        tracker.start()
 
-    def get_features(self, text_csv=None, vision_data=None, vision_csv=None,audio_data=None, audio_csv=None):
-        text_features = None
-        vision_features = None
-        audio_features = None
+        result = func(*args, **kwargs)
 
-        if text_csv is not None:
-            times = []
-            emissions_data = []
-            tracker = EmissionsTracker(project_name=f"Text_feature_extraction")
-            start_time = time.time()
-            tracker.start()
+        elapsed_time = time.time() - start_time
+        total_emissions = tracker.stop()
+        print(80*'==')
+        print(f"⏱ Time elapsed for {func.__name__}: {elapsed_time} seconds")
+        print(f"🌍 Total emissions: {total_emissions} kg CO2")
 
-            text_features = self.text_feature_extractor.extract_bert_features(text_csv)
-            self.text_feature_extractor.save_features_pickle(text_features, self.output_path+'_text_features.pkl')
+        return result
 
-            current_time = time.time() - start_time
-            total_emissions = tracker.stop()
-            times.append(current_time)
-            emissions_data.append(total_emissions)
+    def get_features(self, text_csv=None, vision_data=None, vision_csv=None, audio_data=None, audio_csv=None):
+        if text_csv:
+            features = self.track_emissions_and_time(self.text_feature_extractor.extract_bert_features, text_csv)
+            self.text_feature_extractor.save_features_pickle(features, f"{self.output_path}_text_features.pkl")
 
-        if audio_data is not None and audio_csv is not None:
-            times = []
-            emissions_data = []
-            tracker = EmissionsTracker(project_name=f"Audio_feature_extraction")
-            start_time = time.time()
-            tracker.start()
+        if audio_data and audio_csv:
+            self.track_emissions_and_time(self.audio_feature_extractor.extract_and_save_features, audio_data, audio_csv,
+                                          "AudioData")
 
-            audio_features = self.audio_feature_extractor.extract_and_save_features(audio_data, audio_csv, "AudioData")
-
-            current_time = time.time() - start_time
-            total_emissions = tracker.stop()
-            times.append(current_time)
-            emissions_data.append(total_emissions)
-
-        if vision_data is not None and vision_csv is not None:
-            times = []
-            emissions_data = []
-            tracker = EmissionsTracker(project_name=f"Vision_feature_extraction")
-            start_time = time.time()
-            tracker.start()
-
-            vision_features = self.vision_feature_extractor.extract_and_save_features(vision_data, vision_csv, "VisionData")
-
-            current_time = time.time() - start_time
-            total_emissions = tracker.stop()
-            times.append(current_time)
-            emissions_data.append(total_emissions)
-
-
+        if vision_data and vision_csv:
+            self.track_emissions_and_time(self.vision_feature_extractor.extract_and_save_features, vision_data,
+                                          vision_csv, "VisionData")
 
     def align_features(self):
         pass
@@ -78,6 +57,7 @@ class MultimodalTrainPipeline:
         pass
 
 if __name__ == "__main__":
+    print("🚀 Initializing... [@ 1]")
     pipeline = MultimodalTrainPipeline('./config.ini')
     dataset_text = './feature_extraction/text_features/dev_sent_emo.csv'
     dataset_vision = '/Users/lernmi/Desktop/EmotionUnify/01_Dataset_generation/dataset_adapters/MELD/dev_splits_complete'
@@ -85,15 +65,14 @@ if __name__ == "__main__":
     dataset_audio = dataset_vision
     dataset_audio_csv = dataset_vision_csv
 
-    print("Done [@ 1]")
     pipeline.get_features(
         text_csv=dataset_text,
         vision_data=dataset_vision,
         vision_csv=dataset_vision_csv,
-        audio_data=dataset_audio,  # Assuming you have this
+        audio_data=dataset_audio,
         audio_csv=dataset_audio_csv
     )
-    print("Done [@ 2]")
+    print("🎉 Feature extraction complete! [@ 2]")
     #pipeline.align_features()
     print("Done [@ 3]")
     #pipeline.fusion_features()

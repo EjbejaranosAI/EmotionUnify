58e5abff4befccb08604b31c741687e97f0d847a
Edison <ejbejaranos@gmail.com>
2023-10-20 01:37:03 +0200

    🔧 fix(inference_sample.py): add mapping from predicted class to sentiment to provide more meaningful output ✨ feat(inference_sample.py): add support for displaying the predicted sentiment based on the predicted class

--- 03_Inference_Engine/inference_sample.py
+++ 03_Inference_Engine/inference_sample.py
@@ -32,6 +32,11 @@ def load_models_for_inference(path='./inference_model/weigths'):
 
 
 def make_inference(audio_sample, video_sample, text_sample, text_audio_model, text_video_model, final_model):
+    index_to_sentiment = {
+        0: 'neutral',
+        1: 'positive',
+        2: 'negative'
+    }
     with torch.no_grad():
         # Assuming the sample tensors are already on the same device as the models
         text_audio_fused = text_audio_model(audio_sample, text_sample)
@@ -40,8 +45,9 @@ def make_inference(audio_sample, video_sample, text_sample, text_audio_model, te
 
         # Get the predicted class
         predicted_class = torch.argmax(final_output, dim=1)
+        predicted_sentiment = index_to_sentiment[predicted_class.item()]
 
-    return predicted_class
+    return predicted_class, predicted_sentiment
 
 
 
@@ -49,17 +55,15 @@ def make_inference(audio_sample, video_sample, text_sample, text_audio_model, te
 text_audio_model, text_video_model, final_model = load_models_for_inference()
 
 
-audio_sample_feature = torch.rand(1, 1024)  # Replace with the correct dimensions for your audio data
+# call the feature extraction
+
 video_sample_feature = torch.rand(1, 512)   # Replace with the correct dimensions for your video data
 text_sample_feature = torch.rand(1, 768)    # Replace with the correct dimensions for your text data
+audio_sample_feature = torch.rand(1, 1024)  # Replace with the correct dimensions for your audio data
+
 
 
 
 # Make inference
-# Assuming audio_sample, video_sample, and text_sample are your preprocessed single samples
-predicted_class = make_inference(audio_sample_feature, video_sample_feature, text_sample_feature, text_audio_model, text_video_model, final_model)
-
-
-
-print("\n🎯 Predicted Class 🎯")
-print(predicted_class)
+predicted_class, predicted_sentiment = make_inference(audio_sample_feature, video_sample_feature, text_sample_feature, text_audio_model, text_video_model, final_model)
+print(f"\n🎯The predicted class is {predicted_class.item()} which corresponds to a '{predicted_sentiment}' sentiment.🎯")

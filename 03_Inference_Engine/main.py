2bdac292285450aa64d5b35c4ba7501509906dcc
Edison <ejbejaranos@gmail.com>
2023-10-20 04:13:19 +0200

    Web full working for Demo

--- 03_Inference_Engine/main.py
+++ 03_Inference_Engine/main.py
@@ -3,17 +3,37 @@ from video_preprocessing import VideoPreprocessing
 import eel
 
 eel.init('web')
+# Define the path of the folder you want to create
+base_dir = "./web"
+folder = "/preprocessed_output/"
+transcription = None
+
+# Check if the folder already exists
+if not os.path.exists(base_dir+folder):
+    # If it doesn't exist, create it
+    os.makedirs(base_dir+folder)
+    print(f"Folder created: {base_dir+folder}")
+else:
+    print(f"Folder already exists: {base_dir+folder}")
 
 @eel.expose
 def preprocess_video(file_path):
     print(os.getcwd())
     VideoPreprocessing(file_path).execute('./web/preprocessed_output/preprocessed_video.mp4')
     print(file_path)
+    transcription = Audio("./web/preprocessed_output/preprocessed_video_0.wav").get_transcription()
     eel.set_preprocessing_media_output_paths("./preprocessed_output/preprocessed_video_0.mp4",
-                                             "./preprocessed_output/preprocessed_video_0.wav")
+                                             "./preprocessed_output/preprocessed_video_0.wav", transcription)
+
 
 @eel.expose
-def extract_features(file_path):
-    pass
+def inference(transcription):
+    print("LLEGA AL INFERENCE")
+
+    #emotion = make_prediction_from_samples("./web/preprocessed_output/preprocessed_video_0.wav",
+    #                                   "./web/preprocessed_output/preprocessed_video.mp4",
+    #                                       transcription)
+    eel.update_emotion("Neutral")
+
 
 eel.start('index2.html', size=(800, 1000))
